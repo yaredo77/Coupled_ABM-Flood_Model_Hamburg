f3ad7af2cdb38f6dfe4baad08acea35733384cdf
Yared Abayneh Abebe <yaredo77@yahoo.com>
2020-06-07 12:42:23 +0200

    Add files via upload

---
+++ output/outputcollector.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+sftp -r ubuntu@145.100.57.48 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.48
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.48
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.48
+EOF
+sftp -r ubuntu@145.100.57.62 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.62
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.62
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.62
+EOF
+sftp -r ubuntu@145.100.57.77 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.77
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.77
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.77
+EOF
+sftp -r ubuntu@145.100.57.99 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.99
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.99
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.99
+EOF
+sftp -r ubuntu@145.100.57.100 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.100
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.100
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.100
+EOF
+sftp -r ubuntu@145.100.57.101 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.101
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.101
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.101
+EOF
+sftp -r ubuntu@145.100.57.106 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.106
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.106
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.106
+EOF
+sftp -r ubuntu@145.100.57.127 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.127
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.127
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.127
+EOF
+sftp -r ubuntu@145.100.57.129 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.129
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.129
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.129
+EOF
+sftp -r ubuntu@145.100.57.130 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.130
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.130
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.130
+EOF
+sftp -r ubuntu@145.100.57.131 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.131
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.131
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.131
+EOF
+sftp -r ubuntu@145.100.57.132 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.132
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.132
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.132
+EOF
+sftp -r ubuntu@145.100.57.135 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.135
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.135
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.135
+EOF
+sftp -r ubuntu@145.100.57.136 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.136
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.136
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.136
+EOF
+sftp -r ubuntu@145.100.57.138 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.138
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.138
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.138
+EOF
+sftp -r ubuntu@145.100.57.141 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.141
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.141
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.141
+EOF
+sftp -r ubuntu@145.100.57.142 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.142
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.142
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.142
+EOF
+sftp -r ubuntu@145.100.57.149 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.149
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.149
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.149
+EOF
+sftp -r ubuntu@145.100.57.158 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.158
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.158
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.158
+EOF
+sftp -r ubuntu@145.100.57.161 << EOF
+get /home/ubuntu/simphony_model_1583751388677/instance_1 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.161
+get /home/ubuntu/simphony_model_1583751388677/instance_2 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.161
+get /home/ubuntu/simphony_model_1583751388677/instance_3 /data/Wilhelmsburg_ABM/Wilhelmsburg/output/57.161
+EOF
+
